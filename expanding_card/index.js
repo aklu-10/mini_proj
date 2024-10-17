ef2076b25e611be7bd3558791817402a301f6fee
akash <akashkumar@unicodesystems.info>
2024-10-17 13:59:31 +0530

    fighting game

--- expanding_card/index.js
+++
@@ -1,6 +0,0 @@
-const handleChange = e => {
-    let clickedDiv = e.target;
-    let allItems = document.querySelectorAll('.container-item');
-    [...allItems].map(item=>item.classList.remove('active-item'))
-    clickedDiv.classList.add('active-item')
-}
